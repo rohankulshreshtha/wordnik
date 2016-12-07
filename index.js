0ad1c084658ede77bc0c6c8ab55b1d68ad69547f
rohankulshreshtha <rohankulshreshtha.bkn@gmail.com>
2016-12-07 22:41:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var request = require('request');
 var path = require("path");
 var dict = require('./routes/dict');
 console.log("welcome to command line directory using wordnik apis");
+
+//getting input from the console
+
 process.stdin.setEncoding('ascii');   
 process.stdin.on('data', (input)=>{
 	input = input.trim();
