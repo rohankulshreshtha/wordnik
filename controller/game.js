3112d0fd216383e4f878b9e87a241ea3eb968630
rohankulshreshtha <rohankulshreshtha.bkn@gmail.com>
2016-12-07 17:29:32 +0530

    Add files via upload

---
+++ controller/game.js
@@ -0,0 +1,113 @@
+var path = require("path");
+var api_key = require('../config/key');
+var request = require('request');
+var waterfall = require('async-waterfall');
+module.exports = {
+getCorrect : function (gameObject,check,callback) {
+	request('http://api.wordnik.com:80/v4/word.json/' + gameObject.word + '/relatedWords?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key='+api_key.key, function (error, response, body) {
+    if(error) return console.log('Error while fetching Definition from wordnik API',error);
+    var info = JSON.parse(body);
+    if(info.length == 0) {
+              console.log(`No Definition found`);
+          }
+    else{
+    	for(var syn in info){
+    		if(info[syn].relationshipType=='synonym'){
+    			if(info[syn].words.indexOf(check) != -1){
+    				console.log("right answer");
+    				console.log("try other synonyms");
+    			}
+    			else{ 
+    				console.log("try again");
+    				console.log("enter 1 or 2 or 3");
+    				gameObject.choice=true;
+    			}
+    		}
+    	}
+    }  
+    if(callback) callback(null,word);
+	})
+},
+ getJumbled : function (w){
+ 	 w=w.split('');
+            var n=w.length,
+                jumble =0,
+                temp = 0;
+
+
+            while(n){
+                jumble = parseInt(Math.random() * (n - 1));
+                //swap with last one
+                temp = w[jumble];
+                w[jumble]= w[n-1];
+                w[n-1] = temp;
+                n--;
+            }
+            console.log(w.join(''));
+            console.log("please enter the right answer");
+ },
+ getDefination : function (word,callback) {
+	request('http://api.wordnik.com:80/v4/word.json/' + word + '/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key='+api_key.key, function (error, response, body) {
+    if(error) return console.log('Error while fetching Definition from wordnik API',error);
+    var info = JSON.parse(body);
+    if(info.length == 0) {
+              console.log(`No Definition found`);
+          }
+    else{
+    		console.log(info[0].text+"\n");
+    }  
+    if(callback) callback(null,word);
+	});
+},
+getSynonym : function (word,callback) {
+	var x = false;
+	request('http://api.wordnik.com:80/v4/word.json/' + word + '/relatedWords?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key='+api_key.key, function (error, response, body) {
+		if(error) return console.log('Error while fetching Example from wordnik API',error);
+    var info = JSON.parse(body);
+    if(info.length == 0) {
+              console.log(`No words found`);
+          }
+    else{
+    	for(var syn in info){
+    		if(info[syn].relationshipType=='synonym'){
+    			console.log(info[syn].words[0]);
+    			x = true;
+    		}
+    	}
+    		if(x==false) console.log("no synonyms found");
+    }  
+    if(callback) callback(null,word);
+	})
+},
+getAntonym : function (word,callback) {
+	var x = false;
+	request('http://api.wordnik.com:80/v4/word.json/' + word + '/relatedWords?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key='+api_key.key, function (error, response, body) {
+    if(error) return console.log('Error while fetching Example from wordnik API',error);
+    var info = JSON.parse(body);
+    if(info.length == 0) {
+              console.log(`No words found`);
+          }
+    else{
+    	for(var ant in info){
+    		if(info[ant].relationshipType=='antonym'){
+    			console.log(info[ant].words[0]);
+    			x = true;
+    		}
+    	}
+    	if(x==false) console.log("no antonyms found");
+    }  
+    if(callback) callback(null,word);
+	})
+},
+ getInfo : function(gameObject){
+ 	waterfall([
+         function(callback){
+           module.exports.getDefination(gameObject.word,callback);
+         },
+         module.exports.getSynonym,
+         module.exports.getAntonym
+       ], function(err,result){
+          if(err) return console.log('Error while fetching full dictionary from wordnik API',err);
+       });
+ }
+}
